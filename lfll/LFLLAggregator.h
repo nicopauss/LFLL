9af4fc27ebee9143bac126333e53d4b6444709b1
Nicolas Pauss <nicolas.pauss@gmail.com>
2013-10-21 13:45:12 +0200

    Python generator & licence

--- lfll/LFLLAggregator.h
+++ lfll/LFLLAggregator.h
@@ -1,19 +1,41 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Nicolas Pauss
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 #ifndef LFLLAGGREGATOR_H
 #define LFLLAGGREGATOR_H
 
 #include <cassert>
 
-#include "LFLLDefinitions.h"
-#include "LFLLBinaryOperator.h"
-#include "LFLLMembership.h"
-#include "LFLLConsequence.h"
-#include "LFLLMaxOperator.h"
+#include <lfll/LFLLDefinitions.h>
+#include <lfll/LFLLBinaryOperator.h>
+#include <lfll/LFLLMembership.h>
+#include <lfll/LFLLConsequence.h>
+#include <lfll/LFLLMaxOperator.h>
 
 LFLL_BEGIN_NAMESPACE
 
 /**
  * Aggregate consequences to membership using a binary operator.
- * Defualt is the max operator.
+ * Default is max operator.
  * It is useful if you want to chain up systems.
  */
 template<typename BinaryOperatorType = LFLLMaxOperator>
--- lfll/LFLLStaticAssert.h
+++ lfll/LFLLStaticAssert.h
@@ -20,24 +20,26 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef LFLLMINOPERATOR_H
-#define LFLLMINOPERATOR_H
+#ifndef LFLLSTATICASSERT_H
+#define LFLLSTATICASSERT_H
 
-#include <lfll/LFLLBinaryOperator.h>
+// Adapted static assert from http://stackoverflow.com/a/1980156
 
-LFLL_BEGIN_NAMESPACE
+#define LFLL_CONCATENATE(arg1, arg2)   LFLL_CONCATENATE1(arg1, arg2)
+#define LFLL_CONCATENATE1(arg1, arg2)  LFLL_CONCATENATE2(arg1, arg2)
+#define LFLL_CONCATENATE2(arg1, arg2)  arg1##arg2
 
-/**
-  * Standard min operator
-  */
-struct LFLLMinOperator : public LFLLBinaryOperator
-{
-    inline dom operator()(dom val1, dom val2) const {
-        return (val1 < val2) ? val1 : val2;
-    }
-};
+#define LFLL_STATIC_ASSERT(expression, message)\
+    struct LFLL_CONCATENATE(__static_assertion_at_line_, __LINE__)\
+    {\
+        detail::LFLLStaticAssert<static_cast<bool>((expression))> LFLL_CONCATENATE(LFLL_CONCATENATE(LFLL_CONCATENATE(STATIC_ASSERTION_FAILED_AT_LINE_, __LINE__), _), message);\
+    };\
+    typedef detail::LFLLStaticAssertionTest<sizeof(LFLL_CONCATENATE(__static_assertion_at_line_, __LINE__))> LFLL_CONCATENATE(__static_assertion_test_at_line_, __LINE__)
 
-LFLL_END_NAMESPACE
+namespace detail {
+template<bool> struct LFLLStaticAssert;
+template<> struct LFLLStaticAssert<true> {};
+template<int i> struct LFLLStaticAssertionTest {};
+}
 
-
-#endif //LFLLMINOPERATOR_H
+#endif // LFLLSTATICASSERT_H
