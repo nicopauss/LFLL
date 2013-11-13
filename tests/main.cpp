eaf4a7ff6f241bb491cb9d4f37442eecd21aee33
Nicolas Pauss <nicolas.pauss@etu.utc.fr>
2013-11-13 12:25:46 +0100

    Convert Windows line endings to Unix Line endings

--- tests/main.cpp
+++ tests/main.cpp
@@ -20,14 +20,9 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-/************************************************************************/
-/** @mainpage
-
-Unit tests for the library ElectronicHorizonLFLL.
-
-*/
-/************************************************************************/
-
+/********************************************************************************
+    Unit tests for the library LFLL.
+*********************************************************************************/
 
 #include <gtest/gtest.h>
 
